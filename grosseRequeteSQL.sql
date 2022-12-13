646f390fae10989742c3ffbc7ab63f13faa820f7
Enockii <enocki.9@outlook.fr>
2022-12-13 22:39:03 +0100

    Q6 - buildQuery(début) + grosse requete (sans les WHERE)

--- grosseRequeteSQL.sql
+++ grosseRequeteSQL.sql
@@ -670,3 +670,114 @@ SELECT
       ) AS review ON hotel.ID = review.author
 
       GROUP BY hotel.ID;
+
+
+
+
+/**** V4 distance avec *****/
+
+  /** V3 avec ET cheapestRoom ET les 2 autres **/
+
+  SELECT 
+      hotel.ID AS hotelID,
+      address1Data.meta_value AS address_1,
+      address2Data.meta_value AS address_2,
+      addressCityData.meta_value AS address_city,
+      addressZipData.meta_value AS address_zip,
+      addressCountryData.meta_value AS address_country,
+      CAST(geoLatData.meta_value AS float) AS geo_lat,
+      CAST(geoLngData.meta_value AS float) AS geo_lng,
+      phoneData.meta_value AS phone,
+      emailData.meta_value AS email,
+      coverImageData.meta_value AS coverImage,
+      hotelRoomData.title AS titleRoom,
+      review.rating AS rating,
+      review.ratingCount AS ratingCount
+    
+      FROM tp.wp_users AS hotel
+
+      INNER JOIN tp.wp_usermeta AS address1Data
+        ON hotel.ID = address1Data.user_id AND address1Data.meta_key = 'address_1'
+            
+      INNER JOIN tp.wp_usermeta AS address2Data
+        ON hotel.ID = address2Data.user_id AND address2Data.meta_key = 'address_2'
+    
+      INNER JOIN tp.wp_usermeta AS addressCityData
+        ON hotel.ID = addressCityData.user_id AND addressCityData.meta_key = 'address_city'
+    
+      INNER JOIN tp.wp_usermeta AS addressZipData
+        ON hotel.ID = addressZipData.user_id AND addressZipData.meta_key = 'address_zip'
+    
+      INNER JOIN tp.wp_usermeta AS addressCountryData
+        ON hotel.ID = addressCountryData.user_id AND addressCountryData.meta_key = 'address_country'
+    
+      INNER JOIN tp.wp_usermeta AS geoLatData
+        ON hotel.ID = geoLatData.user_id AND geoLatData.meta_key = 'geo_lat'
+    
+      INNER JOIN tp.wp_usermeta AS geoLngData
+        ON hotel.ID = geoLngData.user_id AND geoLngData.meta_key = 'geo_lng'
+    
+      INNER JOIN tp.wp_usermeta AS coverImageData
+        ON hotel.ID = coverImageData.user_id AND coverImageData.meta_key = 'coverImage'
+    
+      INNER JOIN tp.wp_usermeta AS phoneData
+        ON hotel.ID = phoneData.user_id AND phoneData.meta_key = 'phone'
+    
+      INNER JOIN tp.wp_usermeta AS emailData
+        ON hotel.ID = emailData.user_id AND emailData.meta_key = 'email'
+    
+
+      INNER JOIN (
+        SELECT 
+              post.post_author AS author,
+              post.post_title AS title,
+              MIN(CAST(PriceData.meta_value AS float)) AS price,
+              CAST(SurfaceData.meta_value AS int) AS surface,
+              TypeData.meta_value AS types,
+              CAST(BedroomsCountData.meta_value AS int) AS bedrooms,
+              CAST(BathroomsCountData.meta_value AS int) AS bathrooms,
+              CoverImageData.meta_value AS coverImage
+            
+              FROM tp.wp_posts AS post
+            
+              INNER JOIN tp.wp_postmeta AS SurfaceData
+                ON post.ID = SurfaceData.post_id AND SurfaceData.meta_key = 'surface'
+            
+              INNER JOIN tp.wp_postmeta AS PriceData
+                ON post.ID = PriceData.post_id AND PriceData.meta_key = 'price'
+            
+              INNER JOIN tp.wp_postmeta AS TypeData
+                ON post.ID = TypeData.post_id AND TypeData.meta_key = 'type'
+
+              INNER JOIN tp.wp_postmeta AS BedroomsCountData
+                ON post.ID = BedroomsCountData.post_id AND BedroomsCountData.meta_key = 'bedrooms_count'
+
+              INNER JOIN tp.wp_postmeta AS BathroomsCountData
+                ON post.ID = BathroomsCountData.post_id AND BathroomsCountData.meta_key = 'bathrooms_count'
+            
+              INNER JOIN tp.wp_postmeta AS CoverImageData
+                ON post.ID = CoverImageData.post_id AND CoverImageData.meta_key = 'coverImage'
+
+              WHERE post.post_type = 'room'
+
+              GROUP BY post.post_author
+
+      ) AS hotelRoomData ON hotel.ID = hotelRoomData.author
+
+
+
+      INNER JOIN (
+        SELECT 
+          post_author AS author,
+          ROUND(AVG(CAST(wp_postmeta.meta_value AS UNSIGNED INTEGER))) AS rating, 
+          COUNT(wp_postmeta.meta_value) AS ratingCount
+        FROM wp_usermeta, wp_posts, wp_postmeta
+        WHERE wp_usermeta.user_id = wp_posts.post_author 
+          AND wp_posts.ID = wp_postmeta.post_id 
+          AND wp_postmeta.meta_key = 'rating' 
+          AND wp_posts.post_type = 'review'
+        GROUP BY wp_posts.post_author
+      ) AS review ON hotel.ID = review.author
+
+
+      GROUP BY hotel.ID;
