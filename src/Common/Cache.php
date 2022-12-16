e4e6338b85a51df1b1987125ef7c0dd236ad1d8f
Enockii <enocki.9@outlook.fr>
2022-12-16 23:06:40 +0100

    Q10 - Symfony Cache REDIS

---
+++ src/Common/Cache.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Common;
+
+use Symfony\Component\Cache\Adapter\AdapterInterface;
+use Symfony\Component\Cache\Adapter\RedisAdapter;
+
+class Cache {
+    private static Cache $instance;
+    private AdapterInterface $cache;
+
+    private function __construct() {
+        $redisConnection = RedisAdapter::createConnection('redis://redis');
+        $this->cache = new RedisAdapter($redisConnection, "App\Services\Hotel", 0);
+
+    }
+
+    public static function get() : AdapterInterface {
+        if ( !isset(self::$instance) )
+            self::$instance = new Cache();
+        return self::$instance->cache;
+    }
+}
+
+
+
--- src/index.php
+++ src/index.php
@@ -4,12 +4,15 @@ namespace App;
 
 const __PROJECT_ROOT__  = __DIR__;
 
+use App\Common\Cache;
 use App\Controllers\Hotel\HotelListController;
 use App\Services\Hotel\OneRequestHotelService;
 use App\Services\Hotel\ReworkedHotelService;
 use App\Services\Hotel\UnoptimizedHotelService;
 
 require_once __DIR__ . "/vendor/autoload.php";
+
+Cache::get()->getItem('any_item'); // TODO à retirer après avoir testé !
 
 //$hotelService = UnoptimizedHotelService::getInstance();
 //$hotelService = OneRequestHotelService::getInstance();
